2583a537a3e18d0ced954798da7cb65dfddf1bca
Teddie <teddymachoka98@yahoo.com>
2020-09-06 21:59:37 +0300

    add changes

--- Password-locker/user_credential_test.py
+++ Password-locker/user_credential_test.py
@@ -34,4 +34,37 @@ class TestCredentials(unittest.TestCase):
 	'''
 	Test class that defines test cases for the credentials class behaviours.
 
+    Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+	def test_check_user(self):
+		'''
+		Function to test whether the login in function check_user works as expected
+		'''
+		self.new_user = User('Mary','Ng\'ang\'a','pswd100')
+		self.new_user.save_user()
+		user2 = User('Ken','Ng\'ang\'a','pswd100')
+		user2.save_user()
+
+		for user in User.users_list:
+			if user.first_name == user2.first_name and user.password == user2.password:
+				current_user = user.first_name
+		return current_user
+
+		self.assertEqual(current_user,Credential.check_user(user2.password,user2.first_name))
+
+	def setUp(self):
+		'''
+		Function to create an account's credentials before each test
+		'''
+		self.new_credential = Credential('Mary','Facebook','maryjoe','pswd100')
+
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of credential instances is properly done
+		'''
+		self.assertEqual(self.new_credential.user_name,'Mary')
+		self.assertEqual(self.new_credential.site_name,'Facebook')
+		self.assertEqual(self.new_credential.account_name,'maryjoe')
+		self.assertEqual(self.new_credential.password,'pswd100')
     
